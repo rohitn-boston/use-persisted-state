7c5c4455fc1eb03be534ef73093120dc0160d7c3
Rohith R Nair <rohitn@boston-technology.com>
2024-12-26 02:16:39 -0500

    Update createStorage.js to fix the exception while parsing

--- src/createStorage.js
+++ src/createStorage.js
@@ -1,12 +1,17 @@
 const createStorage = (provider) => ({
   get(key, defaultValue) {
     const json = provider.getItem(key);
-    // eslint-disable-next-line no-nested-ternary
-    return json === null || typeof json === 'undefined'
-      ? typeof defaultValue === 'function'
-        ? defaultValue()
-        : defaultValue
-      : JSON.parse(json);
+    if (json === null || typeof json === 'undefined') {
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+    }
+
+    try {
+      return JSON.parse(json);
+    } catch (error) {
+      console.error(`Error parsing JSON for key "${key}":`, error);
+      // Return defaultValue in case of an error
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+    }
   },
   set(key, value) {
     provider.setItem(key, JSON.stringify(value));
